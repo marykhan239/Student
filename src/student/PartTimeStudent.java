d5bc112850e46e5686f6c0b874ad78d9e756c078
maryam khan <khan521@sheridancollege.ca>
2021-10-06 18:32:29 -0400

    first commit

---
+++ src/student/PartTimeStudent.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package student;
+
+/**
+ *
+ * @author marya
+ */
+public class PartTimeStudent extends Student{
+    
+    private int numberOfCourses;
+    
+    public int getNumOfCourses(){
+        
+        return numberOfCourses;
+    
+   
+}
+    
+    public void setNumOfCourses(int numberOfCourses){
+        
+        
+        this.numberOfCourses=numberOfCourses;
+    }
+    
+}
